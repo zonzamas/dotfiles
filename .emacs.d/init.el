68b97888e55a020f3035e927ce40ec38c5836f5f
Oscar Ferrer <oscar.ferrer@teambox.com>
2014-07-01 11:34:11 +0200

    Pending-delete-mode and rubymode for more files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,4 +9,9 @@
 (eval-after-load "ruby-mode"
   '(add-hook 'ruby-mode-hook 'ruby-electric-mode))
 
+(add-to-list 'auto-mode-alist
+	          '("\\.\\(?:gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist
+	          '("\\(Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
 
+(pending-delete-mode)
