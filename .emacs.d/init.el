b146e4e352539c7251dd376cf12ad70f7743b68a
Oscar Ferrer <zonzamas@gmail.com>
2014-08-22 10:04:35 -0700

    bindings and autocomplete mode everywhere

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,21 +1,25 @@
 ;;(load "cask.el")
-(add-to-list 'load-path "~/.cask/")
 (require 'cask)
 (cask-initialize)
 
 (require 'ido)
 (ido-mode t)
 
-(require 'ruby-electric)
-(eval-after-load "ruby-mode"
-  '(add-hook 'ruby-mode-hook 'ruby-electric-mode))
+;;(require 'ruby-electric)
+;;(eval-after-load "ruby-mode"
+;;  '(add-hook 'ruby-mode-hook 'ruby-electric-mode))
 
 (add-to-list 'auto-mode-alist
-	          '("\\.\\(?:gemspec\\|irbrc\\|gemrc\\|rake\\|erb\\||rb\\|ru\\|thor\\)\\'" . ruby-mode))
+	     '("\\.\\(?:gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist
-	          '("\\(Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
+	     '("\\(Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
+
+(add-to-list 'auto-mode-alist
+	     '("tf" . puppet-mode))
+
 
 (pending-delete-mode)
+(auto-complete-mode)
 
 ;; dirty fix for having AC everywhere
 (define-globalized-minor-mode real-global-auto-complete-mode
@@ -24,4 +28,8 @@
                          (auto-complete-mode 1))
                        ))
 (real-global-auto-complete-mode t)
+
+(global-set-key (kbd "C-c m") 'er/expand-region)
+(global-set-key (kbd "C-c j") 'comment-region)
+(global-set-key (kbd "C-c k") 'uncomment-region)
 
